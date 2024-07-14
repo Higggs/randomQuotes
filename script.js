9fa3f8877c3623c131cd9d63604d5f0b8c9a312f
Higggs <102892736+Higggs@users.noreply.github.com>
2024-07-14 23:28:21 +0530

    Update api

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const quote=document.getElementById("quote");
 const author=document.getElementById("author");
 
-const api_url="https://api.quotable.io/quotes/random";
+const api_url="https://api.quotable.io/random";
 async function getQuote(url){
   const response=await fetch(url);
   var data=await response.json();
